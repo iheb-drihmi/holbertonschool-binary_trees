6b79d8c9df45a1aa0a81d84a997a9037eee63fa5
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-09 23:19:12 +0100

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,7 +5,7 @@
  * @value: the value
  * Return: return
  */
- binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_n;
 
