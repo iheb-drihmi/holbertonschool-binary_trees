03555180974a83c5361e369300c17b69949c9898
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-11 12:27:32 +0100

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - a function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - a function that goes through a binary tree
  * @tree: a tree
  * @func: a function
  * Return: return
