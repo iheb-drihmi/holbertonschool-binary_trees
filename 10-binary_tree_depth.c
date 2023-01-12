20da6a209f27c865dbf7084686825071e68544b4
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-12 23:06:52 +0100

    test10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - a function that measures the depth of a node in a binary tree
+ * binary_tree_depth - a function that measures the depth of a node
  * @tree: tree
  * Return: return
  */
